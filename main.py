f0b875e419c4e43ffdda370929d3cbb6f2c9832b
Ilya <ischetverikov@gmail.com>
2021-10-25 17:55:54 +1000

    create venv

---
+++ main.py
@@ -0,0 +1,9 @@
+print
+
+pip list [-local]
+pip -m venv <Name> [--system-site-packages]
+/Script/activate.bat and deacitvate
+where python
+pip install request
+pip freeze - another from for requests.txt
+pip install -r requests.txt
