b510547985d476a18ecb624140680ca921e188ab
Ilya <ischetverikov@gmail.com>
2021-11-15 22:04:38 +1000

    Appropriate project structure

--- main.py
+++
@@ -1,9 +0,0 @@
-print
-
-pip list [-local]
-pip -m venv <Name> [--system-site-packages]
-/Script/activate.bat and deacitvate
-where python
-pip install request
-pip freeze - another from for requests.txt
-pip install -r requests.txt
